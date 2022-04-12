ea6250b72308585a8e62c5cc0c763d7503087beb
Leonardo Perez <83437994+lenissius12@users.noreply.github.com>
2022-04-11 23:18:57 -0500

    Create main.cpp

---
+++ Hanged_Game/main.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <conio.h>
+#include "game.h" 
+#include <windows.h>
+#include <mmsystem.h>
+
+
+using namespace std;
+
+int main() {
+    game hanged_Game;
+    
+    hanged_Game.Menu(hanged_Game);
+
+
+
+    return 0;
+}
